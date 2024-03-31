b231555f3e3a240ec8f3cb62b1c0e3d58b0d6298
PaniukSerhii <panjuksergy@gmail.com>
2024-04-01 02:10:49 +0300

    Added: new class Hello with method to print hello

---
+++ ConsoleApp1/Hello.cs
@@ -0,0 +1,9 @@
+﻿namespace ConsoleApp1;
+
+public class Hello
+{
+    public void PrintHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+
+var hello = new Hello();
+hello.PrintHello();
